6ce8eb8772feca9588be1065fa5490494defe4b1
Fernando <mhfernando778@gmail.com>
2025-01-31 22:56:44 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,10 @@
+// Task 1 - Product Price Management //
+let prices = [100, 120, 90, 300, 210];
+console.log("Prices:", prices);
+
+prices.push(150);
+console.log("Updated Prices:", prices);
+
+let removedPrices = prices.shift();
+console.log("Prices:", prices);
+
