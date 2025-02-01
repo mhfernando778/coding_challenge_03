1a05d5955d1fce070ce9b22daae597e1e055ea93
Fernando <mhfernando778@gmail.com>
2025-01-31 23:03:25 -0500

    Task 1 - comments added

--- cc_3.js
+++ cc_3.js
@@ -1,10 +1,10 @@
 // Task 1 - Product Price Management //
-let prices = [100, 120, 90, 300, 210];
-console.log("Prices:", prices);
+let prices = [100, 120, 90, 300, 210]; // an array of 5 different prices
+console.log("Prices:", prices); // logging the prices
 
-prices.push(150);
-console.log("Updated Prices:", prices);
+prices.push(150); // adding the price: 150 to the array
+console.log("Updated Prices:", prices); // logging the updated array of prices
 
-let removedPrices = prices.shift();
-console.log("Prices:", prices);
+let removedPrices = prices.shift(); // removing the first price from the array
+console.log("Prices:", prices); // logging the updated array of prices
 
